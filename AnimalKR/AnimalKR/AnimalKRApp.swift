3ecd7eca7cde2f95a5cedc7cea32815ec146b83f
seungyooooong <chadange@naver.com>
2023-12-20 10:29:27 +0900

    Add New Project (AnimalKR)

---
+++ AnimalKR/AnimalKR/AnimalKRApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalKRApp.swift
+//  AnimalKR
+//
+//  Created by 최승용 on 2023/12/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimalKRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalKR/AnimalKR/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AnimalKR
+//
+//  Created by 최승용 on 2023/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
