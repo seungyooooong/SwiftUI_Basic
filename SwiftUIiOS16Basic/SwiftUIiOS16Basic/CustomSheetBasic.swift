2d22c541e14df9169d31920019de338b0b645f8a
seungyooooong <chadange@naver.com>
2024-01-06 15:03:11 +0900

    CustomSheet

---
+++ SwiftUIiOS16Basic/SwiftUIiOS16Basic/CustomSheetBasic.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSheetBasic.swift
+//  SwiftUIiOS16Basic
+//
+//  Created by 최승용 on 1/6/24.
+//
+
+import SwiftUI
+
+struct CustomSheetBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSheetBasic()
+}
